6f380f9eb02e173ef210a4c6e2cd14c6591af38a
Herbert Santos <herbertonline1@hotmail.com>
2024-05-29 15:24:02 -0300

    palavrasecreta

---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,17 @@
+import './StartScreen.css'
+const StartScreen = ({startGame}) =>{
+    return(
+        <>
+        
+        <div className="start">
+            <h1>Palavra Secreta </h1>
+            <p>Clique no botão abaixo para começar o jogo </p>
+            <button onClick={startGame}>Começar o jogo</button>
+           
+        </div>
+
+        </>
+    );
+};
+
+export default StartScreen;
---
+++ src/data/words.jsx
@@ -0,0 +1,16 @@
+// Array com palavras para o jogo
+
+export const wordsList = {
+    Carro: ["Motor", "Porta", "capô", "Pneu", "Antena"],
+    Fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+    Corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+    Computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    Programação: ["Liguagem", "Fremework", "JavaScript", "React"],
+    Alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+  
+
+
+
+
+
+};
